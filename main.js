e2a4921bf86a96501936287482652cdfe9edd9ed
vishal thakur <thecodevishal@gmail.com>
2024-09-18 13:14:23 +0530

    changes

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-    },
-  });
 
  // Function to toggle the mobile menu
 function toggleMenu() {
